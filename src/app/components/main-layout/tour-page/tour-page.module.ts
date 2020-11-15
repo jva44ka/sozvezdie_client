58532092bc61c1c6ef59a62d338d9b6c30713d7d
DESKTOP-5LFPUAC\stalk <zaparivannyi@gmail.com>
2020-11-15 20:04:53 +0300

    Added and styled page for one tour Added spinner Connected library for photo album

--- src/app/components/main-layout/tour-page/tour-page.module.ts
+++ src/app/components/main-layout/tour-page/tour-page.module.ts
@@ -2,14 +2,16 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TourPageComponent } from './tour-page.component';
 import { SharedModule } from '../../../shared/shared.module';
-
-
+import { NgxImageGalleryModule } from 'ngx-image-gallery';
+import { HammerModule } from '@angular/platform-browser';
 
 @NgModule({
   declarations: [TourPageComponent],
   imports: [
     CommonModule,
-    SharedModule
+    SharedModule,
+    NgxImageGalleryModule,
+    HammerModule
   ],
   exports: [TourPageComponent]
 })
