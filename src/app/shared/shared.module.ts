e50d7060ce7c539e860e0d26bd9c330df73232c3
DESKTOP-5LFPUAC\stalk <zaparivannyi@gmail.com>
2020-11-15 20:18:39 +0300

    Removed modal-window component and Material UI

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,27 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TourCardComponent } from './components/tour-card/tour-card.component';
-import { ModalWindowComponent } from './components/modal-window/modal-window.component';
-import { ModalWindowDialogComponent } from './components/modal-window/modal-window-dialog/modal-window-dialog.component';
-import { MatIconModule } from '@angular/material/icon';
 import { RouterModule } from '@angular/router';
 
 
 
 @NgModule({
   declarations: [
-    TourCardComponent,
-    ModalWindowComponent,
-    ModalWindowDialogComponent
+    TourCardComponent
   ],
   imports: [
     CommonModule,
-    RouterModule,
-    MatIconModule
+    RouterModule
   ],
   exports: [
-      TourCardComponent,
-      ModalWindowComponent
+      TourCardComponent
   ]
 })
 export class SharedModule { }
